22f53db6a223db1dde9a0d55b5eeeaf3c18ca7af
yukasansan <64906781+yukasansan@users.noreply.github.com>
2020-06-05 16:01:27 +0900

    マイページ実装途中
    マイページの実装を途中までしました。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  'mypage/index'
   devise_for :users
   root to: 'homes#index'
+  resources :mypage, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
