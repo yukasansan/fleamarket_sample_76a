bd6cbfd47eeb9f9fabd8e404bce6237c2291583d
john03191 <john03tgck01a@gmail.com>
2020-06-11 16:30:46 +0900

    devise-user__error__fix

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,19 @@ module FleamarketSample76a
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.i18n.default_locale = :ja
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.action_view.field_error_proc = Proc.new do |html_tag, instance|
+      if instance.kind_of?(ActionView::Helpers::Tags::Label)
+        # skip when label
+        html_tag.html_safe
+      else
+        "<div class=\"has-error\">#{html_tag}<span class=\"help-block\">#{instance.error_message.first}</span></div>".html_safe
+      end
+    end
+    
   end
 end
+
