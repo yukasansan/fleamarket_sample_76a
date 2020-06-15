8053333b735f937444536f2303ad8fe18ad7a9f6
john03191 <john03tgck01a@gmail.com>
2020-06-15 14:07:37 +0900

    categories.show-delete
    検索機能の実装になりそうなので
    一旦削除して基礎実装に移ります

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,19 +4,4 @@ class CategoriesController < ApplicationController
     @parents = Category.where(ancestry: nil)
   end
 
-  def show
-    @items = @category.set_items
-    @items = @items.where(buyer_id: nil).order("created_at DESC").page(params[:page]).per(9)
-  end
-  
-  private
-  def set_category
-    @category = Category.find(params[:id])
-    if @category.has_children?
-      @category_links = @category.children
-    else
-      @category_links = @category.siblings
-    end
-  end
-
 end
