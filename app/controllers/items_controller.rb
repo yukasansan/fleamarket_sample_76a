d0ae44b8a9ef97819bd838fdee4c2adc29807499
yamachan <64958955+usami-pascal@users.noreply.github.com>
2020-06-16 21:19:21 +0900

    出品機能
    出品データDB登録
    マイグレーションファイル変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,15 +4,28 @@ class ItemsController < ApplicationController
   end
   
   def new
+    @item = Item.new
   end
-  
-  def index
+
+  def create
+    @item= Item.new(item_params)
+    if @item.save!
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   def show
   end
   
   def confirmation
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :size, :condition, :shipping_method, :shipping_days, :prefecture_id, :shipping_price, :price, :text, :category_id, :brand_id, :seller_id, :buyer_id, :sale_status, item_images_attributes: [:image]).merge(seller_id: current_user.id)
   end
 
 end
--- db/migrate/20200610125039_create_items.rb
+++ db/migrate/20200610125039_create_items.rb
@@ -6,15 +6,14 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.string      :condition,        null:false
       t.string      :shipping_method,  null:false
       t.string      :shipping_days,    null:false
-      t.string      :shipping_area,    null:false
+      t.integer     :prefecture_id,    null:false
       t.integer     :shipping_price,   null:false
       t.integer     :price,            null:false
       t.text        :text,             null:false
-      t.integer     :seller_id,        null:false
-      t.integer     :buyer_id
-      t.integer     :brand_id 
-      t.references  :category,         null:false, foreign_key:true
-      t.integer     :prefecture_id,    null:false
+      t.references  :seller
+      t.references  :buyer
+      t.references  :brand 
+      t.references  :category,         foreign_key:true
       t.integer     :sale_status,      null: false, default: 0
       t.timestamps
     end
