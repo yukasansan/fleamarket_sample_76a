9fb4f0caa11246c587da7c98c101edb8cf6916d6
john03191 <john03tgck01a@gmail.com>
2020-06-17 07:45:34 +0900

    リソースの表示
    items_controller
    Itemテーブルから@itemsと@productsに情報を代入する記述
    
    items/index.html.haml
    表示の記述
    
    seed.rbでデータを流すために
    publicに画像を挿入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,10 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
+    @products = Item.all
+    @items = Item.includes(:images).order(updated_at: "DESC")
   end
   
   def new
@@ -8,6 +12,16 @@ class ItemsController < ApplicationController
   
   def show
     @item = Item.find(params[:id])
+  end
+
+  private
+  
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to new_user_session_path unless user_signed_in?
   end
 
 end
