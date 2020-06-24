f7d2c84878ba6fc54a968a1d0af645d0d7adb504
umetake820 <umetac82@gmail.com>
2020-06-24 10:09:19 +0900

    出品ページの画像投稿のビュー調整
    ・カメラアイコンをクリックしてファイルを追加
    ・ファイル選択のボタンを非表示
    ・画像の並びを調整

--- app/assets/javascripts/new_item.js
+++ app/assets/javascripts/new_item.js
@@ -1,46 +1,46 @@
 $(function(){
   let fileIndex = 1
+  // 画像用のinputを生成する関数
   const buildFileField = (num)=> {
     const html = `<div class="js-file_group" data-index="${num}">
-                    <input class="js-file" type="file" 
+                    <input class="js-file" type="file" style="display:none"
                     name="item[item_images_attributes][${num}][image]"
-                    id="item_item_images_attributes_${num}_image">
-                    <span class="js-remove">削除</span>
+                    id="camera[${num}]">
                   </div>`
     fileIndex += 1
     return html;
   }
-
+  // プレビュー用のimgタグを生成する関数
   const buildImg = (index, url)=> {
-    const html = `<img data-index="${index}" src="${url}" width="100px" height="100px" style="margin-right: 18px;" >`
+    const html = `<div class="preview">
+                    <img data-index="${index}" src="${url}" width="100px" height="100px" style="margin-right: 18px;" >
+                    <label for='camera[${index + 1}]'><i class="fa fa-camera"></i></label>
+                    <span class="js-remove">削除</span>
+                    </img>
+                  </div>`
     return html;
   }
 
-
+  $('.hidden-destroy').hide();
 
   $('#image-box').on('change', '.js-file', function(e) {
-
     const targetIndex = $(this).parent().data('index');
+    // ファイルのブラウザ上でのURLを取得する
     const file = e.target.files[0];
+    const blobUrl = window.URL.createObjectURL(file)
+    console.log(blobUrl)
 
-
-    if(!file){
-      $(`.js-file_group[data-index=${targetIndex}]`).find(".js-remove").trigger("click");
-      return false;
-    }
-
-      var blobUrl = window.URL.createObjectURL(file);
-
+    // 該当indexを持つimgタグがあれば取得して変数imgに入れる(画像変更の処理)
     if (img = $(`img[data-index="${targetIndex}"]`)[0]) {
       img.setAttribute('src', blobUrl);
-    } else {
+    } else {  // 新規画像追加の処理
       $('#previews').append(buildImg(targetIndex, blobUrl));
 
     if($(".js-file_group").length >= 10 ){
       return false;
     } else {
+    // fileIndexの先頭の数字を使ってinputを作る
       $('#image-box').append(buildFileField(fileIndex));
-
     }
     }
   });
@@ -48,15 +48,25 @@ $(function(){
   $('#image-box').on('click', '.js-remove', function() {
     let limitFileField = $(".js-file_group:last").data("index");
     const targetIndex = $(this).parent().data('index')
+    // 該当indexを振られているチェックボックスを取得する
     const hiddenCheck = $(`input[data-index="${targetIndex}"].hidden-destroy`);
+    // もしチェックボックスが存在すればチェックを入れる
     if (hiddenCheck) hiddenCheck.prop('checked', true);
+
     $(this).parent().remove();
     $(`img[data-index="${targetIndex}"]`).remove();
-    if ((targetIndex == limitFileField ) || ($(".js-file_group").length >= 9)) ($('#image-box').append(buildFileField(fileIndex)));
+
+    // if ($('.js-file').length == 0) $('#image-box').append(buildFileField(fileIndex[0]));
+    if ((targetIndex == limitFileField ) || ($(".js-file_group").length >= 9)) ($('#image-box').append(buildFileField(fileIndex[0])));
   });
 
-  
-  
+  $('.new-camera').on('click', function(){
+    $('.js-file').click();
+    $('.js-file').change(function() {
+      $('.new-camera').hide();
+    });
+  });
+
   $('#item_price').on("input", function() {
     let input = $("#item_price").val();
     let priceFix = Math.round(input * 0.1)
@@ -69,7 +79,6 @@ $(function(){
     // 販売利益に￥をつける
     $('.selling-profit__zero').prepend('¥')
   });
-
 });
 
 
